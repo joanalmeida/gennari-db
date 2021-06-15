6bfee76b06ae2ceeacd7e8964bf07ec2ebaef630
Joan <joan.almeida@redb.ee>
2021-06-15 18:58:43 -0300

    Product and category tables

---
+++ V1__Create_Product_And_Category.sql
@@ -0,0 +1,31 @@
+IF OBJECT_ID(N'Category', N'U') IS NULL
+    BEGIN
+        CREATE TABLE Category
+        (
+            code varchar(30),
+            name varchar(30),
+            image varbinary,
+            PRIMARY KEY (code)
+        )
+    END
+
+IF OBJECT_ID(N'Product', N'U') IS NULL
+    BEGIN
+        CREATE TABLE Product
+        (
+            id int,
+            name varchar(50),
+            description varchar(100),
+            price money,
+            quantity int,
+            image varbinary,
+            size varchar(10),
+            category varchar(30),
+            PRIMARY KEY (id),
+            CONSTRAINT FK_PRODUCT_CATEGORY
+                FOREIGN KEY (category)
+                    REFERENCES Category (code),
+        )
+    END
+
+GO
