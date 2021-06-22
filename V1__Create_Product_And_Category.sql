49c03c79f06a40541cda8ba0c67e8d8f53604a46
Joan <joan.almeida@redb.ee>
2021-06-22 20:10:38 -0300

    Actualizo la generacion de productos y categorias

--- V1__Create_Product_And_Category.sql
+++ V1__Create_Product_And_Category.sql
@@ -1,11 +1,24 @@
+IF OBJECT_ID(N'Image', N'U') IS NULL
+    BEGIN
+        CREATE TABLE Image
+        (
+            id int IDENTITY(1, 1),
+            image varbinary(MAX),
+            PRIMARY KEY (id)
+        )
+    END
+
 IF OBJECT_ID(N'Category', N'U') IS NULL
     BEGIN
         CREATE TABLE Category
         (
             code varchar(30),
             name varchar(30),
-            image varbinary(MAX),
-            PRIMARY KEY (code)
+            image int,
+            PRIMARY KEY (code),
+            CONSTRAINT FK_CATEGORY_IMAGE
+                FOREIGN KEY (image)
+                    REFERENCES Image (id)
         )
     END
 
@@ -13,12 +26,12 @@ IF OBJECT_ID(N'Product', N'U') IS NULL
     BEGIN
         CREATE TABLE Product
         (
-            id int,
+            id int IDENTITY(1, 1),
             name varchar(50),
             description varchar(100),
             price money,
             quantity int,
-            image varbinary(MAX),
+            image int,
             size varchar(10),
             category varchar(30),
             highlight bit,
@@ -26,6 +39,9 @@ IF OBJECT_ID(N'Product', N'U') IS NULL
             CONSTRAINT FK_PRODUCT_CATEGORY
                 FOREIGN KEY (category)
                     REFERENCES Category (code),
+            CONSTRAINT FK_PRODUCT_IMAGE
+                FOREIGN KEY (image)
+                    REFERENCES Image (id)
         )
     END
 
